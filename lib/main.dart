9d0e10b530a0ce40321b3edce9fed126a05a0a72
Sean Hellum <seanhellum45@gmail.com>
2019-12-02 20:44:44 +0000

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,18 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'Hello',
-            home: Scaffold(
-                appBar: AppBar(
-                    title: Text('Hello World'),
-                ),
-                body: Center(
-                    child: Text('Hello World From Flutter!'),
-                ),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello'),
+        ),
+        body: Center(
+          child: Text('Hello World From Flutter!'),
+        ),
+      ),
+    );
+  }
 }
